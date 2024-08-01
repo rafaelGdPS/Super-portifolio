2b56fe55b69f0a60cc25442437f9e600a4a6a9c6
Rafael <rafaelgermanio@gmail.com>
2024-08-01 13:58:22 -0300

    feat: completando template de profile_details.html

--- projects/serializers.py
+++ projects/serializers.py
@@ -34,12 +34,9 @@ class CertifyingInstitutionsSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def create(self, validated_data):
-        print("1", validated_data)
         certificate_data = validated_data.pop(
             "certificates"
             )
-        print(validated_data)
-        print(certificate_data)
         certifications_institution = CertifyingInstitution.objects.create(
             **validated_data
             )
@@ -47,9 +44,7 @@ class CertifyingInstitutionsSerializer(serializers.ModelSerializer):
             certificate[
                 "certifying_institution"
                 ] = certifications_institution
-            print("2", certificate)
             CertificatesSerializer().create(
                 validated_data=certificate
                 )
-        print(certifications_institution.certificates)
         return certifications_institution
