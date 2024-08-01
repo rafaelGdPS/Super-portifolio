3042e4a6b2b80f57cc38bb64869af1ab0056522c
Rafael <rafaelgermanio@gmail.com>
2024-07-31 21:18:40 -0300

    feat: Implementando rota para tabela projects

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,15 @@
 # from django.shortcuts import render
 
-from projects.serializers import ProfileSerializer
-from .models import Profile
+from projects.serializers import ProfileSerializer, ProjectSerializer
+from .models import Profile, Project
 from rest_framework import viewsets
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
